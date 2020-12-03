b94f6a66290742d9de692e5a05a1271d5d615e8b
nsrinivasanbtech <48716788+nsrinivasanbtech@users.noreply.github.com>
2020-12-03 23:24:56 +0530

    server js change

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static('./dist/doctor-appoinment'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile('./dist/doctor-appoinment/index.html');   
+    res.sendFile('index.html', {root: './dist/doctor-appoinment'});   
 });  
 
 // Start the app by listening on the default Heroku port    
