1473117067ff2b8e5b34167607ea076c7352f027
nsrinivasanbtech <48716788+nsrinivasanbtech@users.noreply.github.com>
2020-12-04 19:31:27 +0530

    remove server js

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/doctor-appoinment'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('./dist/doctor-appoinment/index.html');
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname, '/dist/doctor-appoinment/index.html'));
 });  
 
 // Start the app by listening on the default Heroku port    
